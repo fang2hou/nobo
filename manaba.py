58a28215001482976f9bcc3b7c57a5f37a397274
Zhou Fang <fang2hou@outlook.com>
2018-02-04 21:08:38 +0900

    manaba module version 2
    Rename the project with "nobo"

--- manaba.py
+++ manaba.py
@@ -1,76 +1,78 @@
+# -*- coding: utf-8 -*-
+# Manaba module of nobo (RitsFun API).
+# env: python3
 import requests
+from bs4 import BeautifulSoup as bs
 
-myUsername = "is0000ab"
-myPassword = "12345678"
-myPagePath = "https://ct.ritsumei.ac.jp/ct/home"
+class manabaUser(object):
+    """manabaUser Class"""
+    # Setting for manaba+R
+    manabaHomePagePath = "https://ct.ritsumei.ac.jp/ct/home"
 
-webSession = requests.Session()
+    def __init__(self, username, password):
+        self.rainbowID = username
+        self.rainbowPassword = password
+        self.webSession = requests.Session()
 
-def getValueFromHTML(name, page):
-    pageText = page.text
-    try:
-        tempPageData = pageText.split(name + " value=\"")[1]
-    except:
-        tempPageData = pageText.split(name + "\" value=\"")[1]
-    resultValue = tempPageData.split("\"")[0]
-    return resultValue
+    def login(self):
+        # Load first page using the webSession initialized before
+        firstPage = self.webSession.get(self.manabaHomePagePath)
+        # Initialize the post data for second page
+        secondPagePostData = {
+            "USER": self.rainbowID,
+            "PASSWORD": self.rainbowPassword
+        }
+        # Update the post data using the given information
+        for inputElement in bs(firstPage.text, "html.parser").find_all('input'):
+            tempAttrDict = inputElement.attrs
+            if "name" in tempAttrDict and "value" in tempAttrDict:
+                if tempAttrDict["name"] == "target":
+                    secondPagePostData["target"] = tempAttrDict["value"]
+                elif tempAttrDict["name"] == "smauthreason":
+                    secondPagePostData["smauthreason"] = tempAttrDict["value"]
+                elif tempAttrDict["name"] == "smquerydata":
+                    secondPagePostData["smquerydata"] = tempAttrDict["value"]
+                elif tempAttrDict["name"] == "postpreservationdata":
+                    secondPagePostData["postpreservationdata"] = tempAttrDict["value"]
+        
+        # Load second page
+        secondPage = self.webSession.post("https://sso.ritsumei.ac.jp/cgi-bin/pwexpcheck.cgi", secondPagePostData)
+        # Get the redirect path
+        thirdPagePath = "https://sso.ritsumei.ac.jp" + bs(secondPage.text, "html.parser").find('form').attrs["action"]
+        # Create the post data for second page
+        thirdPagePostData = {
+            "USER": self.rainbowID,
+            "PASSWORD": self.rainbowPassword,
+            "smquerydata": secondPagePostData["smquerydata"],
+        }
 
-def getActionFromHTML(method, page):
-    try:
-        tempPageData = page.text.split("METHOD=\"" + method + "\" ACTION=\"")[1]
-        resultAction = tempPageData.split("\">")[0]
-    except:
-        tempPageData = page.text.split("<form action=\"")[1]
-        resultAction = tempPageData.split("\"")[0]
-    return resultAction
+        # Load third page
+        thirdPage = self.webSession.post(thirdPagePath, thirdPagePostData)
+        # Initialize the post data for the forth page
+        forthPagePostData = {}
+        for inputElement in bs(thirdPage.text, "html.parser").find_all('input'):
+            tempAttrDict = inputElement.attrs
+            if "name" in tempAttrDict and "value" in tempAttrDict:
+                if tempAttrDict["name"] == "RelayState":
+                    forthPagePostData["RelayState"] = tempAttrDict["value"].replace("&#x3a;",":").replace("&#x2f;","/"),
+                elif tempAttrDict["name"] == "SAMLResponse":
+                    forthPagePostData["SAMLResponse"] = tempAttrDict["value"]
+        # Evaluate the page of forth page
+        forthPagePath = bs(thirdPage.text, "html.parser").find('form').attrs["action"].replace("&#x3a;",":").replace("&#x2f;","/")
 
-# 1
-firstPage = webSession.get(myPagePath)
+        # Load forth page
+        forthPage = self.webSession.post(forthPagePath, forthPagePostData)
 
-postFormDataPage1 = {
-    "USER": myUsername,
-    "PASSWORD": myPassword,
-    "target": getValueFromHTML("target", firstPage),
-    "smauthreason": getValueFromHTML("smauthreason", firstPage),
-    "smquerydata": getValueFromHTML("smquerydata", firstPage),
-    "postpreservationdata": getValueFromHTML("postpreservationdata", firstPage),
-}
+    def getCourseList(self):
+        coursePage = self.webSession.get("https://ct.ritsumei.ac.jp/ct/home_course")
+        coursePageCourseTable = bs(coursePage.text, "html.parser").select(".courselist")[0]
 
-# 2
-secondPage = webSession.post("https://sso.ritsumei.ac.jp/cgi-bin/pwexpcheck.cgi", postFormDataPage1)
+        for row in coursePageCourseTable.select(".courselist-c")[1:-2]:
+            print(row.select(".courselist-title")[0].get_text())
 
-actionPage = getActionFromHTML("POST", secondPage)
+        # with open("temp.html", encoding='utf-8', mode='w+') as tempFile:
+        #   tempFile.write(str(courseList))
+        #   tempFile.close()
 
-postFormDataPage2 = {
-    "USER": postFormDataPage1["USER"],
-    "PASSWORD": postFormDataPage1["PASSWORD"],
-    "smquerydata": postFormDataPage1["smquerydata"],
-}
-
-# 3
-thirdPage = webSession.post("https://sso.ritsumei.ac.jp" + actionPage, postFormDataPage2)
-
-
-postFormDataPage3 = {
-    "RelayState": getValueFromHTML("RelayState", thirdPage).replace("&#x3a;",":").replace("&#x2f;","/"),
-    "SAMLResponse": getValueFromHTML("SAMLResponse", thirdPage),
-}
-
-actionPage = getActionFromHTML("POST", thirdPage).replace("&#x3a;",":").replace("&#x2f;","/")
-
-forthPage = webSession.post(actionPage, postFormDataPage3)
-
-
-
-# ----------------------------
-# For debug
-
-with open("D:/temp.html", encoding='utf-8', mode='w+') as tempFile:
-    tempFile.write(forthPage.text)
-    tempFile.close()
-
-# print(webSession.cookies)
-# print(firstPage.text)
-# print(secondPage.text)
-# print(thirdPage.text)
-# print(forthPage.text)
+    def loadCoursePage(self):
+        print(self.webSession.get("https://ct.ritsumei.ac.jp/ct/course_1436639_news_1785319").text)
